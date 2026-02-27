e5ff9c5433ee16e27f509ee4f9d1d1d0e310237a
LeonExist <leon.szameitat.apple2007@gmail.com>
2026-02-27 10:42:25 +0100

    Update App.vue to include FeaturesSection
    - Add FeaturesSection component import
    - Register and display Features component
    - Update component structure

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
 
     <Header />
-
     <Hero />
+    <Features />
 
   </div>
 </template>
@@ -11,12 +11,14 @@
 <script>
 import Header from './components/Header.vue'
 import Hero from './components/HeroSection.vue'
+import Features from './components/FeaturesSection.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Hero
+    Hero,
+    Features
   }
 }
 </script>
